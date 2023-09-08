d8abe03415fe5a0e5f5dc9927237732784e17bc2
Yongfeng LI <wliyongfeng@gmail.com>
2023-09-08 16:50:23 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.TG_TOKEN);
-bot.on('text', (ctx) => ctx.reply('👍'));
-bot.command('/connect', (ctx) => {
+bot.command('connect', (ctx) => {
   console.log(ctx);
   ctx.reply('got command');
 });
+bot.on('text', (ctx) => ctx.reply('👍'));
 
 bot.launch();
