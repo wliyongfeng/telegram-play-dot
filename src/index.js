7d6a43f1784b295a2f2e915fa43bfa921eff9595
Yongfeng LI <wliyongfeng@gmail.com>
2023-09-08 16:48:14 +0800

    Update

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.TG_TOKEN);
 bot.on('text', (ctx) => ctx.reply('👍'));
+bot.command('/connect', (ctx) => {
+  console.log(ctx);
+  ctx.reply('got command');
+});
 
 bot.launch();
